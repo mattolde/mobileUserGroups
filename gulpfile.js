56f1dbc7c7bc9bd68ac1e281c575156b20f396df
Matt Olde <mattolde@gmail.com>
2015-03-17 15:26:13 +1100

    added gulp task for testing

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,11 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var karma = require('gulp-karma');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  jasmine: ['./www/js/*.js']
 };
 
 gulp.task('default', ['sass']);
@@ -25,8 +27,22 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  // gulp.watch(paths.jasmine, ['test']);
 });
 
 gulp.task('install', ['git-check'], function() {
